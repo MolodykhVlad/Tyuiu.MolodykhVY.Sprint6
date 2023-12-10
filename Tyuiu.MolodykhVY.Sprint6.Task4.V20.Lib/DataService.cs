cdc1269d5ee493c678b70f3f6ed46ae96f280260
MolodykhVlad <Mr.Prins@yandex.ru>
2023-12-10 21:38:31 +0300

    Task4

---
+++ Tyuiu.MolodykhVY.Sprint6.Task4.V20.Lib/DataService.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using tyuiu.cources.programming.interfaces.Sprint6;
+
+namespace Tyuiu.MolodykhVY.Sprint6.Task4.V20
+{
+    public class DataService : ISprint6Task4V20
+    {
+        public double[] GetMassFunction(int startValue, int stopValue)
+        {
+            int l = stopValue - startValue + 1;
+            double[] num = new double[l];
+            double m; int r = 0;
+            for (int i = startValue; i <= stopValue; i++)
+            {
+                if ((Math.Cos(i) + 1) == 0) m = 0;
+                else m = Math.Round((Math.Sin(i) - 2 * i) / (3 * i - 1) + Math.Sin(i) - 3 * i + 2, 2);
+                num[r] = m;
+                r++;
+            }
+            return num;
+        }
+    }
+}
---
+++ Tyuiu.MolodykhVY.Sprint6.Task4.V20.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.MolodykhVY.Sprint6.Task4.V20.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.MolodykhVY.Sprint6.Task4.V20.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("e30e1eb9-0f85-4529-993c-4e855a345433")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
